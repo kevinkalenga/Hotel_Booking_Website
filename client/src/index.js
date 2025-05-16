abbbfe412df915f3577a6a9bc7acf617acde6fff
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-16 09:34:37 +0200

    Redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 1. import from react-redux and redux
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux"
+
+// 2. create user reducer function 
+const authReducer = (state = {}, action) => {
+  switch(action.type) {
+    case "LOGGED_IN_USER":
+      return {...state, ...action.payload} 
+    case "LOGOUT":
+        return action.payload;
+    default: 
+       return state; 
+  }
+} 
+
+// 3. combine multiple reducers 
+const rootReducer = combineReducers({
+  user: authReducer,
+}) 
+
+// 4. create redux store
+const store = createStore(rootReducer)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
